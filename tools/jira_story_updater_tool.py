14a47e0bf3da8ddcd51b348de3851ea831f2d1e9
roby73 <rosidotidev@gmail.com>
2025-01-26 01:33:52 +0100

    removed print statement

--- tools/jira_story_updater_tool.py
+++ tools/jira_story_updater_tool.py
@@ -7,6 +7,7 @@ class JiraStoryUpdaterTool(BaseTool):
     name: str = "Jira Update Tool"
     description: str = (
         "This tool allows the user to update a Jira story by providing the story ID and new fields to update."
+        "To update the epic link or to assign a epic to a story uses parent:{'key':epic-key}"
     )
 
     def _run(self, story_id: str, fields: dict) -> str:
@@ -29,9 +30,14 @@ class JiraStoryUpdaterTool(BaseTool):
             # Login
             jira = JIRA(options=jira_options, basic_auth=(username, api_token))
             # Update the story
-            jira.issue(story_id).update(fields=fields)
             if "assignee" in fields.keys():
                 jira.assign_issue(story_id,fields["assignee"])
+                fields.pop("assignee")
+            if "comment" in fields.keys():
+                jira.add_comment(story_id,fields["comment"])
+                fields.pop("comment")
+            jira.issue(story_id).update(fields=fields)
+
             return f"Story {story_id} has been successfully updated."
         except Exception as e:
             return f"Failed to update story {story_id}: {str(e)}"
